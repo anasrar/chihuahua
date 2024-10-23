3faf2e66c648057cf01f3c386c39155f9443f9b7
anasrar <mynameanasrar@gmail.com>
2024-10-24 00:02:56 +1300

    refactor(tim): read raw data instead x y set color

--- pkg/tim2/image.go
+++ pkg/tim2/image.go
@@ -23,13 +23,16 @@ func PictureToImage(picture *Picture) *image.NRGBA {
 		indices = append(indices, data...)
 	}
 
-	img := image.NewNRGBA(image.Rect(0, 0, width, height))
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
-			index := indices[y*width+x]
-			img.Set(x, y, picture.ClutData[index])
-		}
+	raw := []uint8{}
+	for _, index := range indices {
+		c := picture.ClutData[index]
+		raw = append(raw, c.R)
+		raw = append(raw, c.G)
+		raw = append(raw, c.B)
+		raw = append(raw, c.A)
 	}
+	img := image.NewNRGBA(image.Rect(0, 0, width, height))
+	copy(img.Pix, raw)
 
 	return img
 }
